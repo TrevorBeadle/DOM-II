e8cd44b736f666abeca02a8da338ccdaa4b996a2
Trevor Beadle <trevor.beadle.1996@gmail.com>
2020-09-01 10:28:22 -0500

    added event handlers to the nav-links in the header

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+const nav = document.querySelector('.nav');
+const navLinks = nav.querySelectorAll('.nav-link');
+
+// nav-link event handlers
+for (let i = 0; i < navLinks.length; i++){
+    navLinks[i].addEventListener('mouseover', (event) => {
+        event.target.style.color = 'lightgrey';
+    });
+    navLinks[i].addEventListener('mouseout', (event) => {
+        event.target.style.color = 'black';
+    });
+}
